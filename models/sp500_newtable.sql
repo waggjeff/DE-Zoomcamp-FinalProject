74d2f378c66f0ad65f72cb60f4dc45733ce43247
Jeff Wagg <drjeffwagg@gmail.com>
2023-03-16 08:11:55 +0000

    Changed table from nyse to sp500

--- models/sp500_newtable.sql
+++ models/sp500_newtable.sql
@@ -3,9 +3,9 @@
 with newtable as (
     select
         *,
-        100.*(t.open - t.close)/t.open as gain, 
+        100.*(t.Close - t.Open)/t.Open as gain, 
 
-    from `mythic-byway-375404.stocks.nyse` t
+    from `mythic-byway-375404.stocks.sp500` t
 )
 
 select * from newtable
